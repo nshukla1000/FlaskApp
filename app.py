a461e85b8edef2228d1c1fc60e3b21971612ab0a
nshukla1000 <nileshshukla@gmail.com>
2020-05-25 14:02:23 -0500

    a

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 @app.route("/home/<string:name>/<int:id>")
 def hello(name, id):
@@ -8,4 +12,3 @@ def hello(name, id):
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
