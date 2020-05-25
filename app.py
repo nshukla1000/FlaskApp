1b8253dc1852d7fd2c294bba877d7f67d1831680
nshukla1000 <nileshshukla@gmail.com>
2020-05-25 14:48:13 -0500

    a

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 from flask import Flask, render_template
+from mod import sum
 
 app = Flask(__name__)
+all_posts = [
+    {
+        'title' : 'post 1',
+        'content' : 'some content for post 1',
+        'author' : 'Nilesh'
+    },
+    {
+        'title' : 'post 2',
+        'content' : 'some content for post 2'
+    }
+]
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/posts")
+def posts():
+    return render_template("posts.html", posts=all_posts)
+
 @app.route("/home/<string:name>/<int:id>")
 def hello(name, id):
-    return "Hello World 3 " + name + str(id)
+    return "Hello World 3 " + name + str(sum(id, id))
 
 if __name__ == "__main__":
     app.run(debug=True)
