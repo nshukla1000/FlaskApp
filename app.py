60497483a48424350b9e371f1773c1ef11525613
nshukla1000 <nileshshukla@gmail.com>
2020-05-25 13:50:54 -0500

    Initial

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/home/<string:name>/<int:id>")
+def hello(name, id):
+    return "Hello World 3 " + name + str(id)
+
+if __name__ == "__main__":
+    app.run(debug=True)
